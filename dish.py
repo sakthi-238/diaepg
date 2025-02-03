4178159bc8a51d082a8464a60f3305e68e05931f
sakthi-238 <103490048+sakthi-238@users.noreply.github.com>
2025-02-03 14:21:16 +0530

    Update dish.py

--- dish.py
+++ dish.py
@@ -41,38 +41,39 @@ for eppg in epg_json_data:
 	print(f"Channel Name: {eppg['Name']} Channel Id: {eppg['Id']} Status Code: {response.status_code}")
 
 	#print(response.text)
+	if response.status_code == 200:
+		data = response.json()
 
-	data = response.json()
+		def format_time(dt_str):
+			return datetime.strptime(dt_str, "%Y-%m-%dT%H:%M:%S.000Z").strftime("%Y%m%d%H%M%S +0000")
 
-	def format_time(dt_str):
-		return datetime.strptime(dt_str, "%Y-%m-%dT%H:%M:%S.000Z").strftime("%Y%m%d%H%M%S +0000")
+		for channel in data["data"]:
+			channels = {
+					"@id": f"ts{eppg['Id']}",
+					"display-name": eppg["Name"],
+					"icon": {"@src": f"https://api1.viu.lk/api/client/v1/global/images/{eppg['logo_id']}?accessKey=WkVjNWNscFhORDBLCg=="}
+			}
 
-	for channel in data["data"]:
-		channels = {
-				"@id": f"ts{eppg['Id']}",
-				"display-name": eppg["Name"],
-				"icon": {"@src": f"https://api1.viu.lk/api/client/v1/global/images/{eppg['logo_id']}?accessKey=WkVjNWNscFhORDBLCg=="}
-		}
+			channels = {k: v for k, v in channels.items() if v is not None}
+			epg_data["tv"]["channel"].append(channels)
+			for show in channel["shows"]:
 
-		channels = {k: v for k, v in channels.items() if v is not None}
-		epg_data["tv"]["channel"].append(channels)
-		for show in channel["shows"]:
+					programme = {
+							"@start": format_time(show["start"]),
+							"@stop": format_time(show["end"]),
+							"@channel": f"ts{channel['id']}",
+							"@id": str(show["id"]),
+							"title": show["title"],
+							"icon": {"@src": f"https://api1.viu.lk/api/client/v1/global/images/{show['image_id']}?accessKey=WkVjNWNscFhORDBLCg=="}
+					}
 
-				programme = {
-						"@start": format_time(show["start"]),
-						"@stop": format_time(show["end"]),
-						"@channel": f"ts{channel['id']}",
-						"@id": str(show["id"]),
-						"title": show["title"],
-						"icon": {"@src": f"https://api1.viu.lk/api/client/v1/global/images/{show['image_id']}?accessKey=WkVjNWNscFhORDBLCg=="}
-				}
+					programme = {k: v for k, v in programme.items() if v is not None}
+					epg_data["tv"]["programme"].append(programme)
+			
 
-				programme = {k: v for k, v in programme.items() if v is not None}
-				epg_data["tv"]["programme"].append(programme)
-		
-	xml_output = xmltodict.unparse(epg_data, pretty=True)
+xml_output = xmltodict.unparse(epg_data, pretty=True)
 
-	with open("dialog.xml", "w", encoding="utf-8") as xml_file:
-		xml_file.write(xml_output)	
+with open("dialog.xml", "w", encoding="utf-8") as xml_file:
+	xml_file.write(xml_output)	
 
-	#break
+#break
